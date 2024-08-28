8323bf6f70e8d41213051886fff34c86874d482d
Angel Parker <parkeang000@warren.k12.in.us>
2024-08-28 09:55:17 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids = input("how many droids they many to meet:")
+wookiees = input("how many Wookiees they want to meet:")
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
